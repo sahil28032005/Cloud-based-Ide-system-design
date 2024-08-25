eb5b943c99641373738ee23013e01e21b7a8a03f
$@#il <sahilsadekar249775@gmail.com>
2024-08-26 03:14:47 +0530

    #day2

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const pty = require('node-pty');
+const shell = process.platform === 'win32' ? 'powershell.exe' : 'bash';
 
 const app = express();
 const server = http.createServer(app);
@@ -13,6 +15,24 @@ const ioClient = require('socket.io-client');
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
+//pty terminal process setup
+const ptyProcess = pty.spawn(shell, [], {
+    name: 'tty',
+    cols: 80,
+    rows: 30,
+    cwd: process.env.HOME,
+    env: process.env
+
+});
+
+//pty process listener prebuilt
+ptyProcess.on('data', function (data) {
+    console.log(data);
+});
+
+//writer command for testing
+// ptyProcess.write('mkdir abc.txt\r');
+
 //listerning module for socker connectors socket.io connectiions
 io.on('connection', (socket) => {
     console.log("some user connected", socket.id);
@@ -21,6 +41,7 @@ io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         console.log('Message received:', msg);
         io.emit('chat message', msg);
+ // This line is correct if 'msg' is the command you want to send
     });
 
     //spcket disconnection
@@ -32,6 +53,7 @@ io.on('connection', (socket) => {
 app.post('/message', (req, res) => {
     const msg = req.body.message;
     io.emit('chat message', msg);
+    ptyProcess.write(`${msg}\r`);
     res.status(200).send({ status: 'Message sent' });
 });
 
